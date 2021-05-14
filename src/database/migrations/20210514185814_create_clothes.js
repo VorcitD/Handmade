0b84434f22ebfd5379c31950f33e99d024ff7dd0
VorcitD <61586074+VorcitD@users.noreply.github.com>
2021-05-14 16:25:04 -0300

    Configurando Migrations

--- src/database/migrations/20210514185814_create_clothes.js
+++ src/database/migrations/20210514185814_create_clothes.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  knex.schema.createTable('clothes',function(table){
+      table.string('id').primary();
+      table.string('name').notNullable();
+      table.decimal('price').notNullable();
+      table.string('description').notNullable();
+      table.string('gender');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('clothes');
 };
