a9ecd71e4d274097fc2511f5a4a1e964e9e90a77
Campz <rafacampz00@gmail.com>
2021-05-14 17:06:47 -0300

    migrations de users

---
+++ src/database/migrations/20210514192927_create_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users',function(table){
+        table.increments('id');
+        table.string('username').notNullable();
+        table.string('password').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('phone');
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
