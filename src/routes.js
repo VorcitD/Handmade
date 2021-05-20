d5c8a01695e6147ad14b08b61b79ff69809a3a76
Campz <rafacampz00@gmail.com>
2021-05-20 15:11:53 -0300

    update and delete methods in UserController

--- src/routes.js
+++ src/routes.js
@@ -12,5 +12,7 @@ routes.get('/',(request,response)=>{
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 module.exports = routes;
